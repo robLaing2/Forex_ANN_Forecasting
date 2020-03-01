670e43fb69184f7447642d3cf5d86f9926c653c4
leyrpl <leyrpl@exmail.nottingham.ac.uk>
2020-03-01 14:09:54 +0000

    added model implementation into back-end to make predictions

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import tensorflow as tf
+from tensorflow import keras
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 app = Flask(__name__)
 
@@ -20,12 +24,20 @@ colors = [
     "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
 
 
+def predict():
+    new_model = keras.models.load_model('newmodel.h5')
+    x = tf.constant([[-0.99668541, -0.88890724, -0.86432626, -0.87188964, -0.83596358, -0.72062204]])
+    y = new_model.predict(x)
+    return y
+
 
 @app.route('/line')
 def line():
     line_labels=labels
     line_values=values
-    return render_template('graph.html', title='Bitcoin Monthly Price in USD', max=17000, labels=line_labels, values=line_values)
+    prediction = predict()
+    title = 'Prediction: ' + str(prediction)
+    return render_template('graph.html', title=title, max=17000, labels=line_labels, values=line_values)
 
 
 @app.route('/')
