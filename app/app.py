76e5e9e1e81781022306d67ebadeb936f4def728
robLaing2 <robert.laing.15@gmail.com>
2020-05-03 00:52:41 +0100

    deleting of files

--- app/app.py
+++ app/app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-from numpy import zeros, newaxis
 import os
 import quandl
 import pandas as pd
-from collections import OrderedDict
 from dbnomics import fetch_series
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -19,37 +17,9 @@ labels = [
     'Sep', 'Oct', 'Nov', 'Dec'
 ]
 
-values = [
-    967.67, 1190.89, 1079.75, 1349.19,
-    2328.91, 2504.28, 2873.83, 4764.86,
-    4349.29, 6458.30, 9907, 16297
-]
-
-colors = [
-    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
-    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
-    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
-
-input = np.array([1.6386027, 1.60650694, 1.68153863, 1.82628306, 2.02199241, 2.01750265, 1.76344654,
-                      1.76777452,
-                      1.84826674,
-                      1.88859375,
-                      1.97464762,
-                      1.92250969,
-                      1.71640511,
-                      1.49507172,
-                      1.61676059,
-                      1.68930471,
-                      1.62853106,
-                      1.48789214,
-                      1.22168131,
-                      0.94374045])
-
 dataInput = []
 
 START_DATE = '2001-01-01'
-#END_DATE = '2020-02-01'
-
 
 def getMovingAverages(data, windowSize):
     movingAverages = []
@@ -65,7 +35,6 @@ def getMovingAverages(data, windowSize):
         movingAverages.append(average)
 
     return movingAverages
-
 
 EPOCHS = 10
 EVALUATION_INTERVAL = 100
@@ -176,8 +145,6 @@ def predict():
 
     dates, forex = getForex()
 
-    print(dates)
-
     dataDf = pd.DataFrame(columns=['Date','forex','cpi'])
 
     for x in range(len(forex)):
@@ -197,23 +164,7 @@ def predict():
     dataSet = dataDf[features]
     dataSet = dataSet.values
 
-    print(dataSet)
-
-
-    #print(len(forex))
-    #print(len(dates))
-
     recent = dates[-1]
-
-    #b = np.tile(forex, (30, 1))
-    #b = np.array([b])
-    #b = b.reshape(30, 20, 1)
-    #b = tf.constant(b)
-
-    #dataTf = np.tile(dataSet, (30, 1))
-
-    #print(dataSet)
-    #print(dataTf)
     dataTf = tf.constant([dataSet])
 
     y = new_model.predict(dataTf)[0]
@@ -238,9 +189,6 @@ def line():
 
     history = dataInput[0][-6:]
 
-    print("000000")
-    print(history)
-
     history = (history * 0.1634) + 1.308
     predictions = (predictions * 0.1634) + 1.308
 
@@ -250,11 +198,6 @@ def line():
     title = 'Forex Forecast.'
 
     return render_template('graph.html', title=title, max=30, labels=months, history=history, predictions=predictions, recentDate=recentDate)
-
-
-@app.route('/line')
-def hello_world():
-    return render_template('home.html')
 
 
 if __name__ == '__main__':
